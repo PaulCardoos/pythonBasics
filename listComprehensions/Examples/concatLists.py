1cbe4758e18888bbdc4af7aacbad683853a59fcd
paulCardoos <paul.cardoos001@umb.edu>
2021-04-09 00:20:31 -0400

    concat list

---
+++ listComprehensions/Examples/concatLists.py
@@ -0,0 +1,14 @@
+#concatenting lists in python is like combing them 
+
+x = [1, 2, 3]
+y = [4, 5]
+
+z = x + y 
+print(z)
+
+#z = [1, 2, 3, 4, 5]
+
+#if you multiple 3 * x it is similar to x + x + x
+
+print(3 * x)
+#[1, 2, 3, 1, 2, 3, 1, 2, 3]
